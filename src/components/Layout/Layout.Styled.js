1b118e7f30ec4019d82721cd2071c983b4cd3806
VictorFil67 <victorfilinovych@gmail.com>
2024-05-30 18:09:10 +0300

    added adaptive for login

--- src/components/Layout/Layout.Styled.js
+++ src/components/Layout/Layout.Styled.js
@@ -7,14 +7,13 @@ export const Container = styled.div`
 
   @media only screen and (min-width: 768px) {
     max-width: 768px;
-    padding: 0 32px;
+    padding: 32px;
   }
   @media only screen and (min-width: 1280px) {
     max-width: 1280px;
-    padding: 0 40px;
   }
   @media only screen and (min-width: 1440px) {
     max-width: 1440px;
-    padding: 0 64px;
+    /* padding: 0 64px; */
   }
 `;
--- src/components/Login/Login.Styled.js
+++ src/components/Login/Login.Styled.js
@@ -11,8 +11,13 @@ export const EnterWrap = styled.div`
   border-radius: 30px;
   background-color: var(--dark-grey);
   @media only screen and (min-width: 768px) {
-    padding: 40px 64px;
+    padding: 40px 64px 214px;
     gap: 157px;
+  }
+  @media only screen and (min-width: 1280px) {
+    width: 600px;
+    padding: 40px 64px;
+    gap: 107px;
   }
 `;
 export const LogoStyled = styled(Logo)`
@@ -33,14 +38,17 @@ export const AuthSlogan = styled.h1`
   line-height: 1;
   letter-spacing: 0.02em;
   color: var(--white);
+  max-width: 444px;
   @media only screen and (min-width: 768px) {
+    font-size: 64px;
+    line-height: 0.9375;
   }
 `;
 export const AuthSpan = styled.span`
   color: rgba(227, 227, 227, 0.5);
   @media only screen and (min-width: 768px) {
-    font-size: 64px;
-    line-height: 0.9375;
+    /* font-size: 64px;
+    line-height: 0.9375; */
   }
 `;
 
@@ -56,6 +64,7 @@ export const AuthInputWrap = styled.div`
   display: flex;
   flex-direction: column;
   gap: 8px;
+  max-width: 472px;
   @media only screen and (min-width: 768px) {
     gap: 14px;
   }
@@ -65,6 +74,7 @@ export const AuthSubmitBlock = styled.div`
   gap: 14px;
   align-items: center;
   @media only screen and (min-width: 768px) {
+    gap: 20px;
   }
 `;
 export const EmtyBlock = styled.div`
@@ -96,11 +106,16 @@ export const AuthInputTitle = styled.p`
   border-radius: 12px 0 0 12px;
   color: var(--input-title);
   background: var(--input-bg);
-  font: 500 12px / 1.33333 "Gilroy", sans-serif;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 1.33333;
+  /* font: 500 12px / 1.33333 "Gilroy", sans-serif; */
   letter-spacing: -0.02em;
   text-align: center;
   @media only screen and (min-width: 768px) {
-    padding: 16px 16px 16px 0;
+    padding: 16px 0 16px 14px;
+    font-size: 14px;
+    line-height: 1.28571;
   }
 `;
 export const AuthInput = styled.input`
@@ -111,7 +126,10 @@ export const AuthInput = styled.input`
   width: 100%;
   outline: none;
   border: none;
-  font: 500 12px / 1.33333 "Gilroy", sans-serif;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 1.33333;
+  /* font: 500 12px / 1.33333 "Gilroy", sans-serif; */
   letter-spacing: -0.02em;
   &::placeholder {
     background: var(--input-bg);
@@ -124,6 +142,8 @@ export const AuthInput = styled.input`
   }
   @media only screen and (min-width: 768px) {
     padding: 16px 16px 16px 0;
+    font-size: 14px;
+    line-height: 1.28571;
   }
 `;
 export const AuthEyeBtn = styled.button`
@@ -136,9 +156,12 @@ export const AuthEyeBtn = styled.button`
   height: 20px;
   width: 20px;
   z-index: 2;
+  @media only screen and (min-width: 768px) {
+    right: 18px;
+    top: 15px;
+  }
 `;
 export const AuthButton = styled.button`
-  /* margin-top: 22px; */
   border-radius: 30px;
   padding: 12px 15px;
   border: none;
@@ -146,18 +169,34 @@ export const AuthButton = styled.button`
   transition: all 0.3s;
   width: 131px;
   /* white-space: nowrap; */
-  font: 700 14px / 1.28571 "Gilroy", sans-serif;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 1.28571;
+  /* font: 700  / 1.28571 "Gilroy", sans-serif; */
   letter-spacing: 0.02em;
   color: var(--dark-grey);
   &:hover {
     background-color: var(--hover-green);
   }
+  @media only screen and (min-width: 768px) {
+    padding: 16px 54px;
+    width: 166px;
+    font-size: 20px;
+    line-height: 1;
+  }
 `;
 export const LinkStyled = styled(Link)`
-  font: 500 12px / 1.16667 "Gilroy", sans-serif;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 1.16667;
+  /* font: 500 12px / 1.16667 "Gilroy", sans-serif; */
   letter-spacing: -0.02em;
   text-decoration: underline;
   text-decoration-skip-ink: none;
   text-align: center;
   color: var(--input-bg);
+  @media only screen and (min-width: 768px) {
+    font-size: 14px;
+    line-height: 1.28571;
+  }
 `;
