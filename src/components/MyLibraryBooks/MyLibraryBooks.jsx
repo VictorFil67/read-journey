bd42222de2f7b0e22be8f3693e95b5a9963914c3
VictorFil67 <victorfilinovych@gmail.com>
2024-06-25 18:32:04 +0300

    off prev states

--- src/components/MyLibraryBooks/MyLibraryBooks.jsx
+++ src/components/MyLibraryBooks/MyLibraryBooks.jsx
@@ -5,9 +5,9 @@ import bookMobile from "../../images/myLibraryBooksImages/bookMobile.png";
 import bookMobile2x from "../../images/myLibraryBooksImages/bookMobile@2x.png";
 import {
   selectOption,
-  selectPath,
-  selectPrevPath,
-  selectPrevUserBooks,
+  // selectPath,
+  // selectPrevPath,
+  // selectPrevUserBooks,
   selectUserBooks,
   selectfilteredUserBooks,
 } from "../../store/books/selectors";
@@ -26,99 +26,39 @@ import { LibraryItem } from "../LibraryItem/LibraryItem";
 import { useEffect, useState } from "react";
 import { getfilteredUserBooks, setOption } from "../../store/books/booksSlise";
 
-// export const MyLibraryBooks = ({ modalRead, setModalRead }) => {
-//   const userBooks = useSelector(selectUserBooks);
-//   const prevUserBooks = useSelector(selectPrevUserBooks);
-//   const prevPath = useSelector(selectPrevPath);
-//   const path = useSelector(selectPath);
-//   const filteredUserBooks = useSelector(selectfilteredUserBooks);
-//   const option = useSelector(selectOption);
-//   const [selectedOption, setSelectedOption] = useState(option);
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(setOption(selectedOption));
-
-//     if (
-//       selectedOption ||
-//       prevUserBooks.length !== userBooks.length ||
-//       prevPath !== path
-//     ) {
-//       dispatch(getfilteredUserBooks(selectedOption.value));
-//     }
-//   }, [dispatch, selectedOption, prevUserBooks, userBooks, prevPath, path]);
-
-//   return (
-//     <MyLibraryWrap
-//     $length={userBooks?.length}
-//     >
-//       <div>Test543fghfgh21</div>
-//       <TopWrap>
-//         <Title>My library</Title>
-//         <MyBooksFilter
-//           selectedOption={selectedOption}
-//           setSelectedOption={setSelectedOption}
-//         />
-//       </TopWrap>
-//       {userBooks?.length === 0 ? (
-//         <ContentWrap>
-//           <picture>
-//             <source
-//               media="(max-width:767px)"
-//               srcSet={bookMobile + " 1x, " + bookMobile2x + " 2x"}
-//             />
-//             <source srcSet={book + " 1x, " + book2x + " 2x"} />
-//             <Picture src={book} alt="book" loading="lazy" />
-//           </picture>
-//           <Text>
-//             To start training, add <Span>some of your books</Span> or from the
-//             recommended ones
-//           </Text>
-//         </ContentWrap>
-//       ) : (
-//         <MyBookList>
-//           {!selectedOption
-//             ? userBooks.map((book) => (
-//                 <LibraryItem
-//                   key={book._id}
-//                   book={book}
-//                   modalRead={modalRead}
-//                   setModalRead={setModalRead}
-//                 />
-//               ))
-//             : filteredUserBooks.map((book) => (
-//                 <LibraryItem
-//                   key={book._id}
-//                   book={book}
-//                   modalRead={modalRead}
-//                   setModalRead={setModalRead}
-//                 />
-//               ))}
-//         </MyBookList>
-//       )}
-//     </MyLibraryWrap>
-//   );
-// };
-
-export const MyLibraryBooks = () => {
+export const MyLibraryBooks = ({ modalRead, setModalRead }) => {
   const userBooks = useSelector(selectUserBooks);
+  //   const prevUserBooks = useSelector(selectPrevUserBooks);
+  //   const prevPath = useSelector(selectPrevPath);
+  //   const path = useSelector(selectPath);
   const filteredUserBooks = useSelector(selectfilteredUserBooks);
   const option = useSelector(selectOption);
   const [selectedOption, setSelectedOption] = useState(option);
   const dispatch = useDispatch();
-  console.log(selectedOption);
 
   useEffect(() => {
     dispatch(setOption(selectedOption));
-    console.log(selectedOption);
-    if (selectedOption) {
+
+    if (
+      selectedOption
+      // ||
+      // prevUserBooks.length !== userBooks.length ||
+      // prevPath !== path
+    ) {
       dispatch(getfilteredUserBooks(selectedOption.value));
-      console.log(selectedOption.value);
     }
-  }, [dispatch, selectedOption]);
+  }, [
+    dispatch,
+    selectedOption,
+    // prevUserBooks,
+    userBooks,
+    // prevPath,
+    // path
+  ]);
 
   return (
     <MyLibraryWrap $length={userBooks?.length}>
+      <div>Test543fghfgh21</div>
       <TopWrap>
         <Title>My library</Title>
         <MyBooksFilter
@@ -145,13 +85,79 @@ export const MyLibraryBooks = () => {
         <MyBookList>
           {!selectedOption
             ? userBooks.map((book) => (
-                <LibraryItem key={book._id} book={book} />
+                <LibraryItem
+                  key={book._id}
+                  book={book}
+                  modalRead={modalRead}
+                  setModalRead={setModalRead}
+                />
               ))
             : filteredUserBooks.map((book) => (
-                <LibraryItem key={book._id} book={book} />
+                <LibraryItem
+                  key={book._id}
+                  book={book}
+                  modalRead={modalRead}
+                  setModalRead={setModalRead}
+                />
               ))}
         </MyBookList>
       )}
     </MyLibraryWrap>
   );
 };
+
+// export const MyLibraryBooks = ({ modalRead, setModalRead }) => {
+//   const userBooks = useSelector(selectUserBooks);
+//   const filteredUserBooks = useSelector(selectfilteredUserBooks);
+//   const option = useSelector(selectOption);
+//   const [selectedOption, setSelectedOption] = useState(option);
+//   const dispatch = useDispatch();
+//   console.log(selectedOption);
+
+//   useEffect(() => {
+//     dispatch(setOption(selectedOption));
+//     console.log(selectedOption);
+//     if (selectedOption) {
+//       dispatch(getfilteredUserBooks(selectedOption.value));
+//       console.log(selectedOption.value);
+//     }
+//   }, [dispatch, selectedOption]);
+
+//   return (
+//     <MyLibraryWrap $length={userBooks?.length}>
+//       <TopWrap>
+//         <Title>My library</Title>
+//         <MyBooksFilter
+//           selectedOption={selectedOption}
+//           setSelectedOption={setSelectedOption}
+//         />
+//       </TopWrap>
+//       {userBooks?.length === 0 ? (
+//         <ContentWrap>
+//           <picture>
+//             <source
+//               media="(max-width:767px)"
+//               srcSet={bookMobile + " 1x, " + bookMobile2x + " 2x"}
+//             />
+//             <source srcSet={book + " 1x, " + book2x + " 2x"} />
+//             <Picture src={book} alt="book" loading="lazy" />
+//           </picture>
+//           <Text>
+//             To start training, add <Span>some of your books</Span> or from the
+//             recommended ones
+//           </Text>
+//         </ContentWrap>
+//       ) : (
+//         <MyBookList>
+//           {!selectedOption
+//             ? userBooks.map((book) => (
+//                 <LibraryItem key={book._id} book={book} />
+//               ))
+//             : filteredUserBooks.map((book) => (
+//                 <LibraryItem key={book._id} book={book} />
+//               ))}
+//         </MyBookList>
+//       )}
+//     </MyLibraryWrap>
+//   );
+// };
